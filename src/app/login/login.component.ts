3e1cd13ae7f1d5d61cb14ad92fbc34105460180a
KalebeLopes <calbkalebe@gmail.com>
2021-05-07 03:06:38 -0300

    feat: create formGroup on login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router'
 import { LoginService } from '../service/login.service'
 import { HttpErrorResponse } from '@angular/common/http';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { User } from '../Objetos/User'
 
@@ -11,32 +11,45 @@ import { User } from '../Objetos/User'
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
   title = 'project';
-  email: string = ''
-  senha: string = ''
-  autenticado: boolean = false
 
-  user: User = new User('', '', '')
+  loginForm: FormGroup
 
   constructor(
     private router: Router, 
     private route: ActivatedRoute,
     private loginService: LoginService,
-  ) { }
+  ) { 
+    this.loginForm = new FormGroup({
+      email: new FormControl(null, Validators.required),
+      senha: new FormControl(null, Validators.required)
+    })
+  }
 
   ngOnInit(): void {
 
   }
 
   logar = () => {
-    if (this.user.email != '' && this.user.senha != '') {
-      console.log('user ',this.user)
-      this.loginService.logar(this.user).subscribe(res => {
-        console.log('Autenticado')
-        this.autenticado = true
-        this.router.navigate(['venda', this.autenticado])
-      }, (err: HttpErrorResponse) => {
+    // const email: String = this.loginForm.get('email')?.value
+    // const senha: String = this.loginForm.get('senha')?.value
+    
+    console.log(this.loginForm.value)
+    // console.log(senha)
+
+    if (this.loginForm.valid) {
+      this.loginService.logar(this.loginForm.value)
+      .subscribe((res: any) => {
+        // console.log(res.token)
+        if(res.token){
+          localStorage.setItem('token', res.token)
+          console.log(localStorage.getItem('token'))
+          this.router.navigate(['/venda'])
+        }
+      }, 
+      (err: HttpErrorResponse) => {
         alert('Dados inválidos!')
         location.reload()
       })
