297804123a3f86a7042edec5fd3f5d17081ad63a
KalebeLopes <calbkalebe@gmail.com>
2021-05-07 03:08:36 -0300

    feat: hide navbar in unauthorized token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'frontendProject';
   autenticado: boolean = false 
+
+  constructor(
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    this.router.navigate(['/login']) 
+  }
+
+  hideNav(): String {
+    // console.log(this.router.url)
+    return this.router.url
+  }
+
 }
