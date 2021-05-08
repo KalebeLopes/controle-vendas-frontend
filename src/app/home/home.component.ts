5cc2987a70f4e55b17c624b599d2e5f0b646b9c1
KalebeLopes <calbkalebe@gmail.com>
2021-05-08 02:31:34 -0300

    feat: create and implements home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router'
+
+import { LoginService } from '../service/login.service'
+import { VendaService } from '../service/venda.service'
+import { Venda } from '../Objetos/Venda'
+import { QueryBindingType } from '@angular/compiler/src/core';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  vendas: Venda[] = []
+
+  constructor(
+    private logged: LoginService,
+    private vendaService: VendaService,
+    private router: Router
+    ) { }
+
+  ngOnInit(): void {
+    if (this.logged.loggedIn()) {
+      alert('Token expirado')
+      this.router.navigate(['/login'])
+    }
+    this.allSales()
+  }
+
+  allSales(){
+    this.vendaService.getAllSales().subscribe((result) => {
+      if(result.body){
+        this.vendas = result.body
+
+        this.vendas.forEach((obj) => {  // percorre o array de objetos
+          obj.valorTotal = 0
+          obj.qtdTotalItens = 0
+          obj.vendas.forEach((item) => { // percorre o array de itens de cada objeto
+            obj.valorTotal += (item.valor * item.quantidade) // atribui o valor total a cada objeto
+            obj.qtdTotalItens += item.quantidade
+          }) 
+        })
+        // console.log(this.vendas)
+      }
+    })
+  }
+
+}
